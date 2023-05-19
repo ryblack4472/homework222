1589e6e3e419defc5a1885c545544d692deb4e90
ryblack4472 <123537397+ryblack4472@users.noreply.github.com>
2023-05-19 22:32:10 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] input = Console.ReadLine().Split(' ');
+        int N = int.Parse(input[0]);
+        int K = int.Parse(input[1]);
+
+        int[] populations = Array.ConvertAll(Console.ReadLine().Split(' '), int.Parse);
+
+        int maxCustomers = 0;
+
+        for (int i = K; i <= N; i++)
+        {
+            int totalCustomers = 0;
+
+            for (int j = i - K; j <= i + K && j < N; j++)
+            {
+                totalCustomers += populations[j];
+            }
+
+            if (totalCustomers > maxCustomers)
+            {
+                maxCustomers = totalCustomers;
+            }
+        }
+
+        Console.WriteLine(maxCustomers);
+    }
+}
